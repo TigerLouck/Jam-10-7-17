23bba3fbeee1c7e2dd165df58ce4842d0dceb663
TigerLouck <tigerlouck@gmail.com>
2017-10-07 17:44:12 -0400

    Player Controller is usable, though not refined

--- Jam-10-7-17/Jam-10-7-17/Assets/Scripts/PlayerController.cs
+++ Jam-10-7-17/Jam-10-7-17/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+	public int jumpPower;
+	public float runSpeed;
 
+	private bool gravDrawdown = false;
+	private float startGrav;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		startGrav = GetComponent<Rigidbody2D> ().gravityScale;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate ()
+	{
 		
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 ( Input.GetAxis ( "Horizontal" ) * runSpeed, GetComponent<Rigidbody2D> ().velocity.y );
+
+		if (Input.GetKeyDown ( KeyCode.W ))
+		{
+			GetComponent<Rigidbody2D> ().velocity = GetComponent<Rigidbody2D> ().velocity + new Vector2 ( 0, jumpPower );
+			gravDrawdown = true;
+			GetComponent<Rigidbody2D> ().gravityScale = 0;
+		}
+
+		if (gravDrawdown && GetComponent<Rigidbody2D>().gravityScale < startGrav)
+		{
+			GetComponent<Rigidbody2D> ().gravityScale += .05f;
+		}
+		else
+		{
+			gravDrawdown = false;
+		}
 	}
 }
